3d2c0f0f8087474d13dbc72ab8d7922ec76c6e7b
chaperoneg <gmurombo@yahoo.com>
2022-06-12 20:22:10 +0200

    12/6/22

---
+++ productOfMax.js
@@ -0,0 +1,24 @@
+// Introduction and Warm-up (Highly recommended)
+// Playing With Lists/Arrays Series
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 8 * 9 * 10 = 720 .
+// maxProduct ({10, 8, 3, 2, 1, 4, 10}, 5) ==> return (9600)
+function maxProduct(numbers, size){
+    //your code here
+    return numbers.sort((a,b) => b - a).slice(0,size).reduce((acc,c)=> acc * c,1)
+  }
