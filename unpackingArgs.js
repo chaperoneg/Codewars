9c27476ffada16650fd7f9185be0ee16b16d9fdf
chaperoneg <gmurombo@yahoo.com>
2022-08-06 21:26:30 +0200

    06/8/22

---
+++ unpackingArgs.js
@@ -0,0 +1,12 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+function sum(...args) {
+    // return the sum of all arguments given.
+    return args.reduce((acc,c) => acc + c)
+  }
