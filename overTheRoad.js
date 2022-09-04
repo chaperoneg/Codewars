52177c9e31e716ecd36bbda2706ce1dc67f615bc
chaperoneg <gmurombo@yahoo.com>
2022-09-04 20:27:24 +0200

    04/9/22

---
+++ overTheRoad.js
@@ -0,0 +1,6 @@
+// Task
+// You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
+
+function overTheRoad(address, n){
+    return (n * 2) + 1 - address 
+  }
