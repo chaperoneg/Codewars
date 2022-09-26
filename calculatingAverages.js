46cfe465eb02493fb8ed33354c3b5c82c8d0cb32
chaperoneg <gmurombo@yahoo.com>
2022-09-26 16:01:43 +0200

    26/10/22

---
+++ calculatingAverages.js
@@ -0,0 +1,17 @@
+// Let's build a calculator that can calculate the average for an arbitrary number of arguments.
+
+// The test expects you to provide a Calculator object with an average method:
+
+// Calculator.average()
+// The test also expects that when you pass no arguments, it returns 0. The arguments are expected to be integers.
+
+// It expects Calculator.average(3,4,5) to return 4.
+
+var Calculator = {
+    average: function(...args) {
+     // Your code here... 
+      if(args.length === 0) return 0
+      return args.reduce((acc, c) => acc + c,0)/args.length
+      
+    }
+   };
