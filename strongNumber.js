b5ca5a46e4f9eb18fcdef8d04bda20c7825da076
chaperoneg <gmurombo@yahoo.com>
2022-09-27 21:22:45 +0200

    27/10/10

---
+++ strongNumber.js
@@ -0,0 +1,30 @@
+// Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+// For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+// Task
+// Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+
+// Notes
+// Number passed is always Positive.
+// Return the result as String
+// Input >> Output Examples
+// strong_num(1) ==> return "STRONG!!!!"
+// Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+
+// strong_num(123) ==> return "Not Strong !!"
+// Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+
+// strong_num(2)  ==>  return "STRONG!!!!"
+
+function fact(n){
+    if( n <= 1) {return 1}
+    else{
+      return n * fact(n - 1)
+    }    
+  }
+
+function strong(n) {
+  return +n.toString().split("").reduce((acc, c) => +acc + fact(+c),0) === n ? "STRONG!!!!" : "Not Strong !!"
+   
+}
